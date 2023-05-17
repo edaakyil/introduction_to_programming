1eb0aab9211208959834df9d93d6ae641eb485f5
Eda Akyıl <edaakyil@gmail.com>
2023-05-17 21:38:05 +0300

    Create lesson3.java

---
+++ lesson3.java
@@ -0,0 +1,26 @@
+/* May 17 2023 */
+public class Main
+{
+	public static void main(String[] args) {
+		int a, b, c;
+		
+		a = 5;  // 5 --> sabittir(literal)
+		b = 10;   // 10 --> sabittir(literal)
+		c = a + b;
+		
+		System.out.println("Toplam = " + c);  // "Toplam = " --> sabittir(literal)
+		System.out.println("a = " + a + " b = " + b+ " c = " + c);
+		
+		// Noktalı virgül tek başına Boş Deyimdir
+		; // bir tane boş deyim
+		;;  // iki tane boş deyim
+	}
+}
+
+/*
+1) public, static, int --> anahtar sözcük (keyword)
+2) String, args, a, b, c, System, out, println--> isim (identifier)
+3) 5, 10, "Toplam = " --> sabit(literal)
+4) , ; {} --> ayraç
+5) = + () . []  --> operatör
+*/
