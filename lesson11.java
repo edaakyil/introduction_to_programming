6b86bade736ab636519f7a7eb8243b3dd80ca199
Eda Akyıl <edaakyil@gmail.com>
2023-06-14 19:47:18 +0300

    Create lesson11.java

---
+++ lesson11.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+		soru1();
+	}
+	
+        // Soru _ 1 den 10 a kadar sayıları ekrana yazdıran program
+	public static void soru1() {
+	        int i;
+	  
+                for(i = 1; i <= 10; i++)
+                        System.out.println(i);
+		  
+	}
+	
+}
