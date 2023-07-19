9d66bb795a5c81f8c430ce9e99f3db633a60fe84
Eda Akyıl <edaakyil@gmail.com>
2023-07-19 14:12:14 +0300

    Create lesson18.java

---
+++ lesson18.java
@@ -0,0 +1,178 @@
+/* July 13 2023 */
+
+public class Main
+{
+
+        public static void main(String[] args) {}
+        
+        // Soru _ Dizinin en büyük elemanını bulan program
+	public static void soru1() {
+		int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
+		int i, size, max;
+		size = 10;
+		max = a[0];
+		
+		for(i = 1; i < size; i++)
+		        if(max < a[i])
+		                max = a[i];
+		                
+                System.out.println("Dizinin en büyüğü: " + max);
+	}
+
+        // Soru _ Dizinin en büyük ve en küçük elemanlarını bulan program
+	public static void soru2() {
+		int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
+		int i, size, max, min;
+		size = 10;
+		max = a[0];
+		min = a[0];
+		
+		for(i = 1; i < size; i++)
+		        if(max < a[i])
+		                max = a[i];
+	                else if(a[i] < min)
+	                        min = a[i];
+		                
+                System.out.println("Dizinin en büyüğü: " + max);
+                System.out.println("Dizinin en küçüğü: " + min);
+	}
+
+        // Soru _ Dizinin en büyük elemanını ve indis numarasını  bulan program
+	public static void soru3() {
+		int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
+		int i, size, max, p;
+		size = 10;
+		max = a[0];
+		p = 0;   // position
+		
+		for(i = 1; i < size; i++) 
+		        if(max < a[i]) {
+		                max = a[i];
+		                p = i;
+                        }
+		                
+                System.out.println("Dizinin en büyüğü: " + max);
+                System.out.println("Dizinin en büyüğünün indis numarası: " + p);
+	}
+
+        // Soru _ Dizinin en büyük elemanı ile en küçük elemanını yer değiştiren program
+	public static void soru4() {
+		int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
+		int i, size, max, min, pMax, pMin;
+		size = 10;
+		max = a[0];
+		min = a[0];
+		pMax = 0;
+		pMin = 0;
+		
+		for(i = 1; i < size; i++) 
+		        if(max < a[i]) {
+		                max = a[i];
+		                pMax = i;
+	                } else if(a[i] < min) {
+	                        min = a[i];
+		                pMin = i;
+                        }
+                
+                a[pMax] = min;
+                a[pMin] = max;
+		                
+                for(i = 0; i < size; i++)
+                        System.out.print(a[i] +" ");
+	}
+
+        // Soru _ Dizinin ilk elemanı ile en küçük elemanını yer değiştiren program
+	public static void soru5() {
+		int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
+		int i, size, min, pMin;
+		size = 10;
+		min = a[0];
+		pMin = 0;
+		
+		for(i = 1; i < size; i++) 
+		        if(a[i] < min) {
+	                        min = a[i];
+		                pMin = i;
+                        }
+                
+                a[pMin] = a[0];
+                a[0] = min;
+		                
+                for(i = 0; i < size; i++)
+                        System.out.print(a[i] +" ");
+	}
+
+        // Soru _ Dizinin sondan 9 elemanının en küçük ile yine sondan 9 elemanının ilk elemanı yer değiştiren program
+	public static void soru6() {
+		int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
+		int i, size, min, pMin;
+		size = 10;
+		min = a[size - 9];
+		pMin = size - 9;
+		
+		for(i = size - 9; i < size; i++) 
+		        if(a[i] < min) {
+	                        min = a[i];
+		                pMin = i;
+                        }
+                
+                a[pMin] = a[size - 9];
+                a[size - 9] = min;
+		                
+                for(i = 0; i < size; i++)
+                        System.out.print(a[i] +" ");
+	}
+
+        // Soru _ Dizinin elemanlarını küçükten büyüğe doğru sıralayan program
+	public static void soru7() {
+		int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
+		int i, k, size, min, pMin;
+		size = 10;
+		
+		/// Aşağıdaki Algoritmanın adı "Selection Sort" (Seçerek Sıralama) dır.
+		for(k = 0; k < size - 1; k++) {
+        		min = a[k]; 
+        		pMin = k;  
+        		
+        		for(i = k + 1; i < size; i++) 
+        		        if(a[i] < min) {
+        	                        min = a[i];
+        		                pMin = i;
+                                }
+                        
+                        a[pMin] = a[k]; 
+                        a[k] = min;
+                }
+		                
+                for(i = 0; i < size; i++)
+                        System.out.print(a[i] +" ");
+	}
+
+        // Soru _ Dizinin elemanlarını küçükten büyüğe doğru sıralayan program
+        // Her aşaması yazırılsın istersek:
+	public static void soru8() {
+		int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
+		int i, k, size, min, pMin;
+		size = 10;
+		
+		/// Aşağıdaki Algoritmanın adı "Selection Sort" (Seçerek Sıralama) dır.
+		for(k = 0; k < size - 1; k++) {
+        		min = a[k]; 
+        		pMin = k;  
+        		
+        		for(i = k + 1; i < size; i++) 
+        		        if(a[i] < min) {
+        	                        min = a[i];
+        		                pMin = i;
+                                }
+                        
+                        a[pMin] = a[k]; 
+                        a[k] = min;
+                }
+		                
+                for(i = 0; i < size; i++)
+                        System.out.print(a[i] +" ");
+                System.out.println();
+	}
+	
+}
